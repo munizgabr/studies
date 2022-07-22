3d9a0630375e131c4b6dceb8d88efa9ac0fcbd36
munizgabr <gabrielrrtolkien@gmail.com>
2022-07-22 11:22:57 -0300

    Create folder php

---
+++ php/index.php
@@ -0,0 +1,8 @@
+<?php
+
+echo 'O nome enviado é: '.$_POST['name'].' '.$_POST['lastname'].'<br>';
+echo 'O nome enviado é: '.$_POST['name'].'<br>';
+echo 'O email cadastrado foi:'.$_POST['email'];
+
+
+?>
