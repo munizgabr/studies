7417a4a2fec7c08ac0562c16797341bc853a9c11
munizgabr <gabrielrrtolkien@gmail.com>
2022-07-24 22:18:26 -0300

    Learn about JS

--- javascript/src/js/home.js
+++ javascript/src/js/home.js
@@ -77,30 +77,80 @@
 // }
 /*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
-let person = {
-    name: 'Gabriel',
-    age: '24',
-    contaCorrente: 123
-}
+// let person = {
+//     name: 'Gabriel',
+//     age: '24',
+//     contaCorrente: 123
+// }
 
-let operation ={
-    sacar: function(saldo, valor){
-        return saldo -= valor
-    },
-    depositar: function(saldo, valor){
-        return saldo += valor
+// let operation ={
+//     sacar: function(saldo, valor){
+//         return saldo -= valor
+//     },
+//     depositar: function(saldo, valor){
+//         return saldo += valor
+//     }
+// }
+
+// let bank ={
+//     person: person,
+//     operation: operation,
+//     saldo: 1000
+// }
+
+// let value = 10
+
+// console.log(bank.person.name +', portador da conta corrente '+bank.person.contaCorrente + ' realizou um saque no valor de '+ value
+// + ' reais. Seu saldo atual é: R$' +bank.operation.sacar(  
+//     saldo, value)
+// )
+
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+//Métodos de Arrays
+let arr1 = [1,2,3,4,5]
+let arr2 = ['a','b','c']
+
+//Concatenação
+
+let arrFinal = arr1.concat(arr2)
+console.log(arrFinal)
+
+//Every
+// let x = arrFinal.every(
+//     function verifyElement(el, index, obj){
+//         return(typeof el == 'number')
+//     }
+// )
+// console.log(x)
+
+//Filter
+
+// let filterX = arrFinal.filter(
+//     function verifyElement(el, index, obj){
+//         return(el >3)
+//     }
+// )
+// console.log(filterX)
+
+//foreach
+
+
+// let data = ""
+// let forEachX = arrFinal.forEach(
+//     function verifyArray(elem, ind, obj){
+//         if(typeof elem == 'number'){
+//             data += "Índice "+ind+":"+elem+" (este elemento é um número)"+'\n'
+//         }else{
+//             data += "Índice "+ind+":"+elem+" (este elemento não é um número)"+'\n'
+//         }
+//     }
+// )
+// console.log(data)
+
+//indexOf
+
+let indexOfX = arrFinal.indexOf('a')
+    if(indexOfX == -1){
+        alert('Este dado não existe')
     }
-}
-
-let bank ={
-    person: person,
-    operation: operation,
-    saldo: 1000
-}
-
-let value = 10
-
-console.log(bank.person.name +', portador da conta corrente '+bank.person.contaCorrente + ' realizou um saque no valor de '+ value
-+ ' reais. Seu saldo atual é: ' +bank.operation.sacar(  
-    saldo, value)
-)
+    console.log(indexOfX)
