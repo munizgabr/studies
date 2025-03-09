e065650f65d8db112370219850a60682a3b12e75
Gabriel Muniz <gabrielrrtolkien@gmail.com>
2025-03-09 14:52:33 -0300

    create an objetc using factory

---
+++ javascript/src/js/factory/script.js
@@ -0,0 +1,17 @@
+class Person {
+
+    age = 0;
+    
+    constructor(name){
+        this.name = name;
+    }
+}
+
+function createPerson(name, age) {
+    let p = new Person(name);
+    p.age = age;
+    return p;
+}
+
+let p1 = createPerson("João", 31);
+console.log(`${p1.name} tem ${p1.age} anos`);
